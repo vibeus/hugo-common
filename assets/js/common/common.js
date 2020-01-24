a7946cbc8977db5a21f7956398638781d6f6c874
Jiulong Wang <jiulongw@gmail.com>
2020-01-23 17:38:25 -0800

    Make following properties configurable
    1. tablet breakout point
    2. navbar height
    3. heading font

--- assets/js/common/common.js
+++ assets/js/common/common.js
@@ -1,6 +1,4 @@
-// toggle is-active css class when triggerClass element is clicked.
-// onChanged(isActive): callback when is-active is changed.
-export function toggleActive(triggerClass, toggleSelf, onChanged) {
+function bindEventWithTarget(triggerClass, eventName, onEvent) {
   const triggers = document.querySelectorAll(triggerClass);
   triggers.forEach(el => {
     el.addEventListener('click', e => {
@@ -11,14 +9,50 @@ export function toggleActive(triggerClass, toggleSelf, onChanged) {
         return;
       }
 
-      if (toggleSelf) {
-        el.classList.toggle('is-active');
-      }
-
-      target.classList.toggle('is-active');
-      if (onChanged) {
-        onChanged(target.classList.contains('is-active'));
-      }
+      onEvent(el, target, e);
     });
   });
+}
+
+// toggle is-active css class when triggerClass element is clicked.
+// onChanged(isActive): callback when is-active is changed.
+export function toggleActive(triggerClass, toggleSelf, onChanged) {
+  bindEventWithTarget(triggerClass, 'click', (el, target) => {
+    if (toggleSelf) {
+      el.classList.toggle('is-active');
+    }
+
+    target.classList.toggle('is-active');
+    if (onChanged) {
+      onChanged(target.classList.contains('is-active'));
+    }
+  });
+}
+
+// scroll to data-target element (by id) when triggerClass is clicked, with optional extra offset.
+export function bindScrollTo(triggerClass, extraOffset) {
+  bindEventWithTarget(triggerClass, 'click', (el, target) => {
+    const y = target.getBoundingClientRect().top + window.pageYOffset;
+    window.scrollTo({
+      top: y + (extraOffset || 0),
+      behavior: 'smooth',
+    });
+  });
+}
+
+export function debounce(func, wait, immediate) {
+  var timeout;
+  return function(...args) {
+    var context = this;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
