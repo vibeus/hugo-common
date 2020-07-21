0374423b9ed340ed3bd959b84c312e0a152bf5e5
Jiulong Wang <jiulongw@gmail.com>
2020-07-21 14:32:16 -0700

    Fix menu scroll issue for sticky navbar

--- assets/js/common/nav.template.js
+++ assets/js/common/nav.template.js
@@ -3,4 +3,18 @@
 */
 import { toggleActive } from '{{ $src.RelPermalink }}';
 
-toggleActive('.navbar-burger', true);
+toggleActive('.navbar-burger', true, (isActive) => {
+  const navbar = document.querySelector('.navbar');
+  if (navbar.classList.contains('is-fixed-top')) {
+    return;
+  }
+
+  // For navbar that use position: sticky, we need to calculate menu size manually.
+  const menu = document.getElementById('nav-menu');
+  const navbarTop = navbar.getBoundingClientRect().top;
+  if (isActive) {
+    menu.style.maxHeight = `calc(100vh - ${navbarTop}px - 4.125rem)`;
+  } else {
+    menu.style.maxHeight = '';
+  }
+});
