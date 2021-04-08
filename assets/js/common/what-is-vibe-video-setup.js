1bac72b5803004c75c6b084fe5feaf9adc6aa9c6
ScottHAZI <45495507+ScottHAZI@users.noreply.github.com>
2021-04-08 21:12:11 +0800

    New templates (#41)

---
+++ assets/js/common/what-is-vibe-video-setup.js
@@ -0,0 +1,23 @@
+/*
+{{ $src := resources.Get "js/common/common.js" | resources.Minify | resources.Fingerprint }}
+*/
+
+import { bindScrollTo, bindEventWithTarget } from '{{ $src.RelPermalink }}';
+
+bindEventWithTarget('.is-what-is-vibe .is-playlist-item', 'click', (el) => {
+  const bodys = el.parentElement.querySelectorAll('.body');
+  bodys.forEach(body => {
+    body.style.height = body.closest('.is-playlist-item') == el ? body.scrollHeight + 'px' : '0px';
+  });
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector('.is-playlist-item').click();
+});
+
+const navbar = document.querySelector('.navbar');
+const navbarHeight = navbar ? navbar.clientHeight : 0;
+bindScrollTo(
+  '.is-request-demo',
+  -navbarHeight
+);
