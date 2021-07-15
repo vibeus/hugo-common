a9e83cc97e56933229afac3cb0068ee220e017f6
dzr975336710 <975336710@qq.com>
2021-07-15 17:53:25 +0800

    lp edu and bus page

--- assets/js/common/demo.template.js
+++ assets/js/common/demo.template.js
@@ -5,29 +5,35 @@ const navbar = document.querySelector('.navbar.is-fixed-top');
 const navbarHeight = navbar ? navbar.clientHeight : 0;
 bindScrollTo('.is-request-demo', -navbarHeight);
 
-document.querySelectorAll('.form.is-vibe-form').forEach((el) => {
-  setupForm(el, {
-    'click.after': (name, type, form) => {
-      if (name === 'request-demo') {
-        form.parentElement.classList.add('is-live-demo');
-        form.parentElement.classList.remove('is-video-demo');
-        form.parentElement.classList.remove('is-contact-sales');
-      } else if (name == 'request-video') {
-        form.parentElement.classList.add('is-video-demo');
-        form.parentElement.classList.remove('is-live-demo');
-        form.parentElement.classList.remove('is-contact-sales');
-      } else if (name == 'contact-sales') {
-        form.parentElement.classList.add('is-contact-sales');
-        form.parentElement.classList.remove('is-live-demo');
-        form.parentElement.classList.remove('is-video-demo');
-      }
-
-      return false;
-    },
-    'submit.after': (name, type, form) => {
-      form.closest('.section.is-book-demo').classList.add('is-submitted');
-
-      return false;
-    },
+if (document.querySelectorAll('.form.is-vibe-form.is-book-demo-new')) {
+  setupForm(document.querySelector('.form.is-vibe-form'));
+} else {
+  document.querySelectorAll('.form.is-vibe-form').forEach((el) => {
+    setupForm(el, {
+      'click.after': (name, type, form) => {
+        if (name === 'request-demo') {
+          form.parentElement.classList.add('is-live-demo');
+          form.parentElement.classList.remove('is-video-demo');
+          form.parentElement.classList.remove('is-contact-sales');
+        } else if (name == 'request-video') {
+          form.parentElement.classList.add('is-video-demo');
+          form.parentElement.classList.remove('is-live-demo');
+          form.parentElement.classList.remove('is-contact-sales');
+        } else if (name == 'contact-sales') {
+          form.parentElement.classList.add('is-contact-sales');
+          form.parentElement.classList.remove('is-live-demo');
+          form.parentElement.classList.remove('is-video-demo');
+        }
+  
+        return false;
+      },
+      'submit.after': (name, type, form) => {
+        form.closest('.section.is-book-demo').classList.add('is-submitted');
+  
+        return false;
+      },
+    });
   });
-});
+}
+
+
