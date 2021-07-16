7e4ee42cc18b973230f77984c0687e25e68b7364
dzr975336710 <975336710@qq.com>
2021-07-16 15:20:24 +0800

    lp business and education page

---
+++ assets/js/common/open-live-demo-scheduler.js
@@ -0,0 +1,20 @@
+/*
+{{ $src := resources.Get "js/common/common.js" | resources.Minify | resources.Fingerprint }}
+*/
+import { openDemoScheduler, toggleActive } from '{{ $src.RelPermalink }}';
+
+toggleActive('.modal-background, .modal-close', false);
+const btn = document.querySelector(
+  'section.is-book-demo-new .cta .button.is-open-live-demo-scheduler'
+);
+if (btn) {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    openDemoScheduler(
+      null,
+      '{{ .modalId }}',
+      'https://meetings.hubspot.com/jian-zhao/vibe-follow-up'
+    );
+  });
+}
